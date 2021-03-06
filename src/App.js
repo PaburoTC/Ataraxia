0b6643cece571adcbaa36c6150deb5f6bf0bb03e
PaburoTC <pablotcampos25@gmail.com>
2021-03-06 18:23:15 +0100

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        ATARAXIA BABYYYYYY
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
